f2538eb2fe6db6da0cafb579ea25cbacc7c64ab8
Ashley-Nicole18 <ashleynicole.gellado-23@cpu.edu.ph>
2025-12-12 10:44:36 +0800

    chore: modify frontend url to prepare for deployment

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -3,7 +3,7 @@ import { useAuth } from "../context/AuthContext"
 import { useNavigate } from "react-router-dom"
 
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/api"
 
 export default function Login() {
     const { login } = useAuth()
