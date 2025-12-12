d73bd0055d9a5f50d85ad0f39a8d6c66fe5915cf
Ashley-Nicole18 <ashleynicole.gellado-23@cpu.edu.ph>
2025-12-12 09:57:43 +0800

    chore: configure pool.ts for deployment

--- server/src/db/pool.ts
+++ server/src/db/pool.ts
@@ -3,13 +3,24 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
+const connectionString = process.env.DATABASE_URL
+
 const pool = new Pool({
+
+    connectionString : connectionString
+    ? connectionString
+    : undefined,
+
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT) || 5432
+    port: Number(process.env.DB_PORT) || 5432,
+
+    ssl: connectionString ? { rejectUnauthorized: false } : undefined
+
 })
+
 
 pool.on('error', (err, client) => {
     console.error('Unexpected error on idle client',err);
